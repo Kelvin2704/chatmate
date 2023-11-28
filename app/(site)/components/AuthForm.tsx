4a78ef44bd5b42da6ce2d89ae33199d4e31e3d7a
Kelvin2704 <locnguyenphuocloc67@gmail.com>
2023-11-28 19:58:05 +0700

    Task4_NextAuth_Register

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -7,6 +7,7 @@ import { useForm, FieldValues, SubmitHandler } from "react-hook-form"
 import Button from "../../components/Button"
 import AuthSocialButton from "./AuthSocialButton"
 import { BsGithub,BsGoogle } from 'react-icons/bs'
+import axios from "axios"
 
 type Variant = "LOGIN" | "REGISTER"
 
@@ -41,6 +42,7 @@ const AuthForm = () => {
 
         if (variant === 'REGISTER') {
             //axios register
+            axios.post('/api/register',data)
         }
         if (variant === 'LOGIN') {
             //nextauth signin
